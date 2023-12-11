42494c925c45f05ab00a84406523b655eba10358
TTedy <breki121212@gmail.com>
2023-12-11 00:00:24 +0000

    11.12.2023

---
+++ lokaverkefni-kodi/journeynode.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "journey.hpp"
+#include "journeynode.hpp"
+
+struct JourneyNode {
+    Journey* nr;
+    JourneyNode* next;
+
+    JourneyNode(Journey* nr) {
+        this->nr = nr;
+        this->next = nullptr;
+    }
+
+    ~JourneyNode() {
+        delete this->nr; // Corrected the variable name to match the member variable
+    }
+};
